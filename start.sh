ee7604343ededca1b4c49dac7b2dd5218e872999
lihuajun <lhj12345678>
2021-06-02 17:12:46 +0800

    123

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-d=${date '+%Y-%m-%d %H:%m'}
-docker build -t blog-rocketmq:${d} .
-docker run -d --name blog-rocketmq -p 8089:8089 blog-rocketmq:${d}
+d=`date '+%Y-%m-%d %H:%m'`
+docker build -t blogrocketmq:${d} .
+docker run -d --name blog-rocketmq -p 8089:8089 blogrocketmq:${d}
 
