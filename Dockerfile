121ad28bd26973611d10e76b5f525ab031e52f14
lihuajun <lhj12345678>
2021-06-02 17:45:41 +0800

    123

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM jingyuzhao/jdk1.8
+FROM docker.io/ascdc/jdk8
 COPY target/blog-rocketmq-1.0-SNAPSHOT.jar /app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-tag=`date '+%Y-%m-%d %H:%m'`
+tag=`date '+%s'`
 docker build -t blogrocketmq:${tag} .
 docker run -d --name blog-rocketmq -p 8089:8089 blogrocketmq:${tag}
-
